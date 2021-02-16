5ff8078e8217e868044d2fde9c48c8c773de76f5
Chitra Sharathchandra <vigilant.hum@gmail.com>
2021-02-15 23:23:07 -0500

    Updates to visualizations and add presentation

--- data manipulation and visualization/discountrx_analysis_helper.py
+++ data manipulation and visualization/discountrx_analysis_helper.py
@@ -7,3 +7,9 @@ def max_within_group(df):
     maxPrice_stores=df.loc[df.groupby('Drug name')['Price'].idxmax()]
     maxPrice_counts = maxPrice_stores['Store Type'].value_counts()
     return maxPrice_counts
+
+def store_type_color_palette(dat):
+    return ["#a6cee3" if x=='Department Store' else 
+                          ('#1f78b4' if x=="Grocery Chain" else 
+                           ("#b2df8a" if x=="Local Pharmacy" else 
+                            ("#33a02c" if x== "Pharmacy Chain" else "#fb9a99"))) for x in dat]
